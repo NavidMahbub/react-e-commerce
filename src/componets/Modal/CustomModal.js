db59339f770991180805b4a31b646516ea56bfbb
NavidMahbub <navidmahbub27@gmail.com>
2021-09-23 18:00:20 +0600

    custom hooks Added

---
+++ src/componets/Modal/CustomModal.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function CustomModal() {
+    return (
+        <div>
+            <div>Header</div>
+            
+            
+        </div>
+    )
+}
